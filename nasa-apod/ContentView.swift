d4669723bb96f88314992b1e60e51637445a5662
dunnoconnor <michael.dunn.oconnor@gmail.com>
2023-07-12 13:29:49 -0400

    Initial Commit

---
+++ nasa-apod/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nasa-apod
+//
+//  Created by Michael Dunn-OConnor on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nasa-apod/nasa_apodApp.swift
@@ -0,0 +1,17 @@
+//
+//  nasa_apodApp.swift
+//  nasa-apod
+//
+//  Created by Michael Dunn-OConnor on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct nasa_apodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
